7dfa645dd0a02242f824be758eaa20839ca35937
Tochi Vera Abara <tochiabara@icloud.com>
2022-04-05 16:39:17 +0100

    feat: updated files

---
+++ dist/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Container from './components/Container/Container';
+import Header from './components/Header/Header';
+import './data/icons';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Container />
+    </div>
+  );
+}
+
+export default App;
